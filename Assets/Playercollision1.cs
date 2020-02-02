8125bad2c4248fae326bda6046b6fa853b7a4de9
F0gboy <Jasp3r@live.dk>
2020-02-02 11:22:40 +0100

    ok? collison virker

---
+++ Assets/Playercollision1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Playercollision1 : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    public GameManager gameManager;
+
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
