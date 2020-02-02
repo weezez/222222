a9474cbada0427572eac659584699e4c541e9bd8
Magnus <magnus@fotorummet.dk>
2020-02-02 12:48:12 +0100

    Menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
--- Assets/scripts/LevelComplete.cs
+++ Assets/scripts/LevelComplete.cs
@@ -1,10 +1,1 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class LevelComplete : MonoBehaviour
-{
-    public void LoadNextLevel () { 
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-}
+﻿
