747b7015609c027a5c56518402779401e4e02c34
Magnus <magnus@fotorummet.dk>
2020-02-02 10:41:20 +0100

    As

---
+++ Assets/Playercollision2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class playercollision : MonoBehaviour
+{
+
+    public playermovement movement;
+    public GameManager gameManager;
+
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
