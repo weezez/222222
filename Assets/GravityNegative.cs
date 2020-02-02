4b1866713529b59a6d1ab2fb83ea944dc1093243
Magnus <magnus@fotorummet.dk>
2020-02-02 03:23:15 +0100

    Scrips update

---
+++ Assets/GravityNegative.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityNegative : MonoBehaviour
+{
+    void Example()
+    {
+        Physics.gravity = new Vector3(0, -1.0F, 0);
+    }
+}
