a638a890e7202827542a2321a43d7d3bae149424
F0gboy <Jasp3r@live.dk>
2020-02-02 10:38:12 +0100

    kurlyjoe?

---
+++ Assets/Respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private AudioSource aS;
+
+    public bool soundActive;
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+        if (soundActive)
+            aS.Play();
+    }
+
+}
